330488c153d7c3f14e4e7edb132446ace1925fd0
hsh <hanssangh@naver.com>
2022-10-23 16:47:31 +0900

    bindActionCreators 사용

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -8,20 +8,12 @@ const CounterContainer = ({number, increase, decrease}) => {
     );
 };
 
-const mapStateToProps = state => ({
-    number: state.counter.number
-});
-
-const mapDispatchToProps = dispatch => ({
-    increase: () => {
-        dispatch(increase());
-    },
-    decrease: () => {
-        dispatch(decrease());
-    }
-});
-
 export default connect(
-    mapStateToProps,
-    mapDispatchToProps,
+    state => ({
+        number: state.counter.number
+    }),
+    {
+        increase,
+        decrease
+    }
 )(CounterContainer);
